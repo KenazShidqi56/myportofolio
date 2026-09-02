7ebb7fa04405e5ef5b34a2a1f026ee9695dd927f
KenazShidqi <kenaz.shidqi@ui.ac.id>
2026-09-02 17:16:21 +0700

    whitenoise added

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,6 +82,8 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 # tutorial 1...
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
